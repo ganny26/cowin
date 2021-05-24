ce36364e4971d5ce0966f53e6ac786c95866caf7
srajam077 <selvaganesh_rajam@comcast.com>
2021-05-24 13:27:54 +0530

    Age limit

--- src/App.js
+++ src/App.js
@@ -4,14 +4,32 @@ import './styles.css';
 
 const Session = (props) => {
   return (
-    <div style={{ display: 'flex', alignItems: 'center' }}>
-      <div className="ms-2">
+    <div style={{ display: 'flex', alignItems: 'center', border: '1px solid pink' }}>
+      <div className="ms-2" style={{ width: 200 }}>
         <span class="badge rounded-pill bg-primary">{props.vaccine}</span>
       </div>
-      <div className="ms-2">{props.min_age_limit} years</div>
+
       {/* <div className="ms-2">Available: {props.available_capacity}</div> */}
-      <div className="ms-2">Dose1: {props.available_capacity_dose1}</div>
-      <div className="ms-2">Dose2: {props.available_capacity_dose2}</div>
+      <div style={{ display: 'flex', flexDirection: 'column' }}>
+        <div style={{ display: 'flex', margin: 10 }}>
+          <div style={{ width: 100 }}>
+            Dose 1 <br />
+            {props.available_capacity_dose1}
+          </div>
+          <div style={{ width: 100, borderLeft: '1px solid #ccc' }}>
+            Dose 2
+            <br /> {props.available_capacity_dose2}
+          </div>
+        </div>
+        <div className="ms-2">
+          <b>{props.min_age_limit} years</b>
+        </div>
+      </div>
+      <div>
+        <div>Available Date</div>
+        <br />
+        {props.date}
+      </div>
     </div>
   );
 };
@@ -28,6 +46,7 @@ const COWIN_API_URL = (districtId, date) =>
 export default function App() {
   let [loading, setLoading] = React.useState(true);
   let [hdata, setData] = React.useState([]);
+  let [ageLimit, setAgeLimit] = React.useState('18');
   let districtList = [
     {
       name: 'chennai',
@@ -60,11 +79,11 @@ export default function App() {
       let apolloList = data1.filter((item) => item.name.toLowerCase().includes('apollo'));
       console.log('apolloList', apolloList);
       setData(apolloList);
+      setLoading(false);
     };
 
     fetchAllHospitalDetails();
-    setLoading(false);
-  }, []);
+  }, [ageLimit]);
 
   return (
     <div className="container">
@@ -74,13 +93,50 @@ export default function App() {
         ) : (
           <div className="container">
             <h2>Hospital Details</h2>
+            <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
+              <input
+                onClick={(e) => {
+                  console.log('18', e.target.name);
+                  setAgeLimit(e.target.name);
+                }}
+                type="radio"
+                class="btn-check"
+                name="18"
+                id="btnradio1"
+                autocomplete="off"
+                checked={ageLimit === '18'}
+              />
+              <label class="btn btn-outline-primary" for="btnradio1">
+                18+ Years
+              </label>
+
+              <input
+                type="radio"
+                class="btn-check"
+                name="45"
+                id="btnradio2"
+                autocomplete="off"
+                onClick={(e) => {
+                  console.log('45', e.target.name);
+                  setAgeLimit(e.target.name);
+                }}
+                checked={ageLimit === '45'}
+              />
+              <label class="btn btn-outline-primary" for="btnradio2">
+                45+ Years
+              </label>
+            </div>
 
             <table class="table">
               <thead>
                 <tr>
-                  <th scope="col">Hospital Name</th>
-                  <th scope="col">Address</th>
-                  <th scope="col">Session</th>
+                  <th scope="col" style={{ width: '16.66%' }}>
+                    Hospital Name
+                  </th>
+                  <th scope="col" style={{ width: '18.66%' }}>
+                    Address
+                  </th>
+                  <th>Session</th>
                 </tr>
               </thead>
               <tbody>
@@ -90,7 +146,7 @@ export default function App() {
                     <td>{item.address}</td>
                     <td>
                       {item.sessions.map((ctx) => (
-                        <Session {...ctx} />
+                        <Session ageLimit={ageLimit} {...ctx} />
                       ))}
                     </td>
                   </tr>
